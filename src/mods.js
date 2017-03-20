b27b0a77ddd3722041d9d4996af35ae4dc20ca31
GloriousYellow <yellowglorious@gmail.com>
2017-03-20 16:35:13 -0400

    Add MultiHotbar

--- src/mods.js
+++ src/mods.js
@@ -53,4 +53,5 @@ export default [
   'https://addons-origin.cursecdn.com/files/2392/608/generators-0.9.20.23-mc1.10.2.jar',
   'https://addons-origin.cursecdn.com/files/2392/611/pressure-1.3.1.25-mc1.10.2.jar',
   'https://addons-origin.cursecdn.com/files/2392/598/bdlib-1.12.4.24-mc1.10.2.jar',
+  'https://addons-origin.cursecdn.com/files/2391/951/multihotbar-1.10.2-2.1.3-build-T49.jar'
 ]
