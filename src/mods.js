c936d3c152dfff41bc49847fdb84778944b5dbb3
GloriousYellow <yellowglorious@gmail.com>
2017-03-19 20:00:50 -0400

    Remove Refined Storage

--- src/mods.js
+++ src/mods.js
@@ -6,7 +6,6 @@ export default [
   'https://addons-origin.cursecdn.com/files/2357/416/jei_1.10.2-3.13.6.392.jar',
   'https://addons-origin.cursecdn.com/files/2353/326/Mantle-1.10.2-1.1.3.jar',
   'http://chickenbones.net/maven/codechicken/CodeChickenLib/1.10.2-2.5.6.239/CodeChickenLib-1.10.2-2.5.6.239-universal.jar',
-  'https://addons-origin.cursecdn.com/files/2359/687/refinedstorage-1.2.13.jar',
   'https://addons-origin.cursecdn.com/files/2353/329/TConstruct-1.10.2-2.6.1.jar',
   'https://addons-origin.cursecdn.com/files/2359/908/ExtremeReactors-1.10.2-0.4.5.22.jar',
   'https://addons-origin.cursecdn.com/files/2347/456/BiomesOPlenty-1.10.2-5.0.0.2109-universal.jar',
